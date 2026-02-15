5a60100cc32c73dc929d7f15c00dc3cacef4b221
Дмитрий Ценеков <49730476+DimaThenekov@users.noreply.github.com>
2026-02-15 14:38:14 +0300

    add proxy

--- src/engine/modules/MMV86.ts
+++ src/engine/modules/MMV86.ts
@@ -56,6 +56,7 @@ export default class V86Module implements MachineModule {
       net_device: {
         type: 'virtio',
         relay_url: 'fetch',
+        cors_proxy: 'https://cors-anywhere.com/'
       },
       disable_speaker: true,
       autostart: true,
